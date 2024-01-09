5f91534188aed148393445f5a8b62a24230917cd
okc06058 <okc06058@naver.com>
2024-01-09 12:16:15 +0900

    수정

--- src/main/java/com/psychopath/dogstalking/dto/UserDto.java
+++ src/main/java/com/psychopath/dogstalking/dto/UserDto.java
@@ -15,6 +15,6 @@ public class UserDto {
     private String email;
     private String phone;
     private LocalDate birthdate;
-    private String image;
+    private String images;
     private LocalDateTime created_at;
 }
