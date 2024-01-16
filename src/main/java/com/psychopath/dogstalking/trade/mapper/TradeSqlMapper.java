b47e797eb0c0632a4c8822adf777c02b7241bfb5
Pyeonggeun <rjal3054@gmail.com>
2024-01-16 10:17:34 +0900

    trade

--- src/main/java/com/psychopath/dogstalking/trade/mapper/TradeSqlMapper.java
+++ src/main/java/com/psychopath/dogstalking/trade/mapper/TradeSqlMapper.java
@@ -3,9 +3,14 @@ package com.psychopath.dogstalking.trade.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import java.util.List;
 
 import com.psychopath.dogstalking.dto.DogDto;
+import com.psychopath.dogstalking.dto.UserDto;
 import com.psychopath.dogstalking.trade.dto.CareDogDto;
+import com.psychopath.dogstalking.trade.dto.CarePriceDto;
+import com.psychopath.dogstalking.trade.dto.ChatMessageDto;
+import com.psychopath.dogstalking.trade.dto.ChatRoomDto;
 import com.psychopath.dogstalking.trade.dto.TradeArticleDto;
 
 @Mapper
@@ -18,4 +23,41 @@ public interface TradeSqlMapper {
     public int getRecentTradeArticlePk();
 
     public void insertCareDog(CareDogDto careDogDto);
+
+    public void updateTradeArticle(TradeArticleDto tradeArticleDto);
+
+    public void insertCarePrice(CarePriceDto carePriceDto);
+
+    //돌봄 글 리스트
+    public List<TradeArticleDto> getArticleList();
+
+    // 유저 정보
+    public UserDto getUserDto(int userPk);
+
+    // 돌봄가격
+    public int getCarePriceByArticlePk(int articlePk);
+
+    // 돌봄 강아지
+    public List<CareDogDto> getCareDogListByArticlePk(int articlePk);
+
+    // 돌봄 글
+    public TradeArticleDto getArticleDto(int articlePk);
+
+    // 채팅방 생성
+    public void insertChatRoomDto(ChatRoomDto chatRoomDto);
+
+    // 생성된 채팅방 pk 가져오기
+    public int getRecentChatRoomPk();
+
+    // 메시지 등록
+    public void insertChatMessage(ChatMessageDto chatMessageDto);
+
+    // 채팅방 목록 가져오기
+    public List<ChatRoomDto> getChatRoomList(int userPk);
+
+    // 채팅 메시지 리스트 가져오기
+    public List<ChatMessageDto> getChatMessageByChatRoomPk(int chatRoomPk);
+
+    // 채팅 메시지 읽음으로 업데이트
+    public void updateChatMessageIsRead(int messagePk);
 }
