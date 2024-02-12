c5dee61d772cfff50f28acf119d70582600c7e59
okc06058 <okc06058@naver.com>
2024-02-13 06:08:35 +0900

    update diagnosis

---
+++ src/main/java/com/psychopath/dogstalking/diagnosis/controller/DiagnosisController.java
@@ -0,0 +1,35 @@
+package com.psychopath.dogstalking.diagnosis.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.psychopath.dogstalking.diagnosis.service.DiagnosisServiceImpl;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/diagnosis")
+public class DiagnosisController {
+
+    @Autowired
+    private DiagnosisServiceImpl diagnosisService;
+
+    @RequestMapping("diagnosisMainPage")
+    public String diagnosisMainPage(HttpSession session) {
+        return "diagnosis/diagnosisMainPage";
+    }
+
+    @RequestMapping("diagnosisSkinPage")
+    public String diagnosisSkinPage(HttpSession session) {
+        return "diagnosis/diagnosisSkinPage";
+    }
+
+   @RequestMapping(value = "/diagnosisSkinResultPage", method = RequestMethod.GET)
+    public String diagnosisSkinResultPage(HttpSession session) {
+        return "diagnosis/diagnosisSkinResultPage";
+    }
+    
+
+}
---
+++ src/main/java/com/psychopath/dogstalking/diagnosis/mapper/DiagnosisSqlMapper.java
@@ -0,0 +1,8 @@
+package com.psychopath.dogstalking.diagnosis.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DiagnosisSqlMapper {
+    
+}
