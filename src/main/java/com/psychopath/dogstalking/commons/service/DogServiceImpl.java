2a586dbf7ffb371c776c4d9b2271fb28a4753773
Pyeonggeun <rjal3054@gmail.com>
2024-02-21 20:40:37 +0900

    dd

--- src/main/java/com/psychopath/dogstalking/commons/service/DogServiceImpl.java
+++ src/main/java/com/psychopath/dogstalking/commons/service/DogServiceImpl.java
@@ -2,9 +2,12 @@ package com.psychopath.dogstalking.commons.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 import com.psychopath.dogstalking.commons.mapper.DogSqlMapper;
 import com.psychopath.dogstalking.dto.DogDto;
+
+import jakarta.servlet.http.HttpSession;
 
 @Service
 public class DogServiceImpl {
@@ -17,5 +20,16 @@ public class DogServiceImpl {
         dogMapper.insertDog(dogDto);
     }
 
+    public List<DogDto> getDogList(int userPk){
+
+
+        return dogMapper.getDogList(userPk);
+    }
+
+
+    public DogDto getDogInfo(int dogPk){
+
+        return dogMapper.getDogInfo(dogPk);
+    }
 
 }
